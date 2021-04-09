ca38db4a07492e7acc00cd06b3a4b0f4b47cad46
Alex Meta <alexmeta517@gmail.com>
2021-04-09 09:43:53 +0300

    Trial Correct Sitemap

--- Remedy1/views.py
+++ Remedy1/views.py
@@ -36,7 +36,7 @@ def log(request):
 
         if user is not None:
             login(request, user)
-            return redirect('upload')
+            return redirect('Remedy1:upload')
         else:
             messages.info(request, 'Username Or Password Is INCORRECT!!!')
 
